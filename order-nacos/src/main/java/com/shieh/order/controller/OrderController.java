80ed2254bbf1fe898cc4c1086ce1eefe63b4e512
shieh <shieh789@163.com>
2022-02-10 10:39:17 +0800

    问题修改

--- order-nacos/src/main/java/com/shieh/order/controller/OrderController.java
+++ order-nacos/src/main/java/com/shieh/order/controller/OrderController.java
@@ -2,22 +2,27 @@ package com.shieh.order.controller;
 
 import com.shieh.order.feign.StockFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/order")
+@RefreshScope
 public class OrderController {
 
     @Autowired
     StockFeignService stockFeignService;
 
+    @Value("${user.password}")
+    private String configStr;
 
     @RequestMapping("/queryOrder")
     public String queryOrder(){
         String msg = stockFeignService.reduceStock();
-        return "查询订单"+msg;
+        System.out.println("配置文本:"+configStr);
+        return "查询订单"+msg+configStr;
     }
-
 }
--- stock-nacos/src/main/java/com/shieh/stock/controller/StockController.java
+++ stock-nacos/src/main/java/com/shieh/stock/controller/StockController.java
@@ -1,11 +1,15 @@
 package com.shieh.stock.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/stock")
 public class StockController {
+
+
 
     @RequestMapping("/reduce")
     public String reduceStock(){
